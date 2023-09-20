e843b424f3a54b3cf22a81476a0e2e7e7956197b
Moibaty <Moibaty@DESKTOP-B8S6THB>
2023-09-20 16:48:17 +0300

    lesson 20/09/2023

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿int n, i = 1;
+﻿/*
+int n, i = 1;
 n = int.Parse(Console.ReadLine());
 
 while (i < n)
@@ -15,6 +16,148 @@ else
 {
     Console.WriteLine("Это не является степенью тройки");
 }
+*/
+
+//20.09.2023
+//#1
+/*
+int k,f = 1, s = 0;
+k = int.Parse(Console.ReadLine());
+for (int i = 1;i <= k;i+=1)
+{
+    f *= i;
+    s += f;
+}
+
+Console.WriteLine(s);
+*/
+
+//#2
+/*
+double x = double.Parse(Console.ReadLine());
+double s = 0;
+double m = 1;//x^
+double l = 1;
+for ( int k = 1; k < 10; k++)
+{
+    s += m / l;
+    m *= x;
+    l *= k;
+}
+Console.WriteLine(s);
+Console.WriteLine(Math.Exp(x));
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
