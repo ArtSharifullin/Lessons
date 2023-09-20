0ed7e91787425373d799f03f84f06b06535c3512
Moibaty <Moibaty@DESKTOP-B8S6THB>
2023-09-20 15:55:50 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿int n, i = 1;
+n = int.Parse(Console.ReadLine());
+
+while (i < n)
+{
+    i *= 3;
+}
+
+if (i == n)
+{
+    Console.WriteLine("Это степень тройки");
+}
+
+else
+{
+    Console.WriteLine("Это не является степенью тройки");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
